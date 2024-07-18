9e1a4761de98f25493572daaddc221f17725b778
Michel Murabito <michelmurabito@Mac-mini-di-Michel.local>
2024-07-18 17:39:10 +0200

    auto-update

--- _next/static/-b24lzJ4gUMj2RTB8kyIF/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fabout-me","\u002Farticles","\u002Farticles\u002F[slug]","\u002Farticles\u002Fpage\u002F[page]","\u002Fprojects","\u002Fspeaking","\u002Fspeaking\u002F[slug]","\u002Fthank-you","\u002Fuses"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()

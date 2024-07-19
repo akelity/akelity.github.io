115b50b069d84b588b164d07f33dc7c25c91add5
Mich Murabito <mich.murabito@gmail.com>
2024-07-20 00:12:59 +0200

    optimize output

---
+++ _next/static/tY7GbO9usmEYCn7_c48Mq/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fabout-me","\u002Farticles","\u002Farticles\u002F[slug]","\u002Fprojects","\u002Fspeaking","\u002Fspeaking\u002F[slug]","\u002Fthank-you","\u002Fuses"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
